2a58cf8cf762d4ab98d81bfa2bbda23e78a413ad
sky_chao <tian_chao163@163.com>
2018-01-08 20:40:07 +0800

    car

---
+++ www/app/columns/people/PeopleIndex.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import App from "../../App";
+
+export default class PeopleIndex extends Component {
+    render() {
+        return (
+            <App>
+                <div>
+                    <h1></h1>
+                </div>
+            </App>
+        )
+    }
+}
